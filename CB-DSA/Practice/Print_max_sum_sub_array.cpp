3da47b8c3b8c0664ac99c01e5f89a3119278494e
Devaanshraheja <148315570+Devaanshraheja@users.noreply.github.com>
2025-04-26 21:17:25 +0530

    push

--- CB-DSA/Practice/Print_max_sum_sub_array.cpp
+++ CB-DSA/Practice/Print_max_sum_sub_array.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
 #include<climits>// this is for using int_min int_max
 using namespace std;
-void print_subarray_with_max_sum(int a[], int){
+void print_subarray_with_max_sum(int a[], int n ){
     int max_sum=INT_MIN;
+    int wi, wj;
     for (int i = 0; i < n; i++)
     {
         for (int k = i; k<n; k++)
         {
             int sum=0;
-            for (int m = i; m<j; i++)
+            for (int m = i; m<=k; m++)
             {
                 sum=sum+a[m];
+               
             }
-            if(sum>max_sum) max_sum=sum;
+            if(sum>max_sum) {
+                max_sum=sum;
+                wi=i;
+                wj=k;
+            }
+
             
         }
         
     }
+    for (int i = wi; i <=wj; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    
     
 }
-int main()
+int main(){
+    int a[]={51,1,2,3,56,4,-1,};
+    int n=sizeof(a)/sizeof(int);
+    print_subarray_with_max_sum(a,n);
+    return 0;
+}
