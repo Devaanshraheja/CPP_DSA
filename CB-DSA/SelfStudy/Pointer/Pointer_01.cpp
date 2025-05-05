5c1f57b17df48ec6000791bd9ec56b85c42481e7
Devaanshraheja <148315570+Devaanshraheja@users.noreply.github.com>
2025-05-05 17:25:38 +0530

    pushing practice

---
+++ CB-DSA/SelfStudy/Pointer/Pointer_01.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+ int main(){
+    int arr[5]={9,1,2,3,4};
+    cout<<*(arr+1)<<endl;// this gives the element at index 1
+    cout<<*(arr+1)<<endl;// this is the dereference operator
+    char a[]={'a', 'n','c'};
+    cout<<*(a)<<endl;
+
+ }
